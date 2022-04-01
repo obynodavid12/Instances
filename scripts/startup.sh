9928f04b18c4585877cefca97386a60c317fa78e
obynodavid12 <obynodavid12@gmail.com>
2022-04-01 16:04:37 -0400

    made adjust

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-apt update -y
+cat <<EOF >/home/ubuntu/user-data.sh
+#!/bin/bash
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 sudo cp jq /usr/bin
 curl --request POST 'https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token' --header "Authorization: token ${personal_access_token}" > output.txt
 runner_token=\$(jq -r '.token' output.txt)
-mkdir /home/ubuntu/actions-runner && cd /home/ubuntu/actions-runner || exit
-curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
-tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
-chown -R ubuntu /home/ubuntu/actions-runner
-rm /home/ubuntu/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz
-REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-sudo -i u ubuntu bash << EOF
-/home/ubuntu/actions-runner/config.sh --url https://github.com/obynodavid12/Instances/ --token \$runner_token --name "DEV-TEST-SELFHOSTED-RUNNER" --unattended
+mkdir ~/actions-runner
+cd ~/actions-runner
+curl -o actions-runner-linux-x64-2.289.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.1/actions-runner-linux-x64-2.289.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.289.1.tar.gz
+rm ~/actions-runner/actions-runner-linux-x64-2.289.1.tar.gz
+~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances/ --token \$runner_token --name "DEV-TEST-SELFHOSTED-RUNNER" --unattended
+~/actions-runner/run.sh
 EOF
-echo "Configured"
-sudo ./svc.sh install
-echo "Installed"
-sudo ./svc.sh start
-echo "Started"
+cd /home/ubuntu
+chmod +x user-data.sh
+bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
+sudo ./svc install
+sudo ./svc start
