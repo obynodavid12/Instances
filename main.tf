b6d267024075a2cb6c6fd79c5ab9593f8dabb5fc
obynodavid12 <obynodavid12@gmail.com>
2022-04-01 12:23:54 -0400

    removed labels

--- main.tf
+++ main.tf
@@ -41,7 +41,6 @@ module "ssh-key" {
 module "ec2" {
   source     = "./modules/ec2"
   namespace  = var.namespace
-  labels     = var.labels
   vpc        = module.networking.vpc
   sg_pub_id  = module.networking.sg_pub_id
   sg_priv_id = module.networking.sg_priv_id
