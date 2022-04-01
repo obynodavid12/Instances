160e30bf5acdeca9f1119a0b7bd5f0799293651f
obynodavid12 <obynodavid12@gmail.com>
2022-04-01 12:26:54 -0400

    removed label

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo -u ubuntu mkdir /home/ubuntu/actions-runner
 sudo -u ubuntu curl -o /home/ubuntu/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
 sudo -u ubuntu tar xzf /home/ubuntu/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz -C /home/ubuntu/actions-runner
-sudo -u ubuntu EC2_INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id` bash -c 'cd /home/ubuntu/actions-runner/;./config.sh --url ${github_repo_url} --pat ${personal_access_token} --name "${runner_name}-$${EC2_INSTANCE_ID}" --work _work --labels ${labels} --runasservice'
+sudo -u ubuntu EC2_INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id` bash -c 'cd /home/ubuntu/actions-runner/;./config.sh --url ${github_repo_url} --pat ${personal_access_token} --name "${runner_name}-$${EC2_INSTANCE_ID}" --runasservice unattended --replace'
 
 # start the github runner as a service on startup
 cd /home/ubuntu/actions-runner/;./svc.sh install
