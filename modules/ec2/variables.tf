d6175a9bfde9ca1ec0d48239556338068466dc76
obynodavid12 <obynodavid12@gmail.com>
2022-03-31 21:35:24 -0400

    Updated variable

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -20,6 +20,6 @@ variable "sg_priv_id" {
 
 variable "personal_access_token" {
   type = string
-  description = "personal token of the runner repo"
-  default = ""
+  description = "The GitHub Personal token that would be used by the GitHub Runner to authenticate with GitHub Repo"
+  
 }
