4e8cbae5d3bfa0d5b4f4d93999dda628f86733c0
obynodavid12 <obynodavid12@gmail.com>
2022-04-01 12:01:32 -0400

    amended templatefile

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -18,8 +18,27 @@ variable "sg_priv_id" {
   type = any
 }
 
+
+variable "github_repo_url" {
+  description = "The GitHub Repo URL for which the GitHub Runner to be registered with"
+  type        = string
+  default     = ""
+}
+
 variable "personal_access_token" {
-  type = string
-  description = "The GitHub Personal token that would be used by the GitHub Runner to authenticate with GitHub Repo"
-  default = ""  
+  description = "The GitHub Repo Pat Token that would be used by the GitHub Runner to authenticate with the GitHub Repo"
+  type        = string
+  default     = ""
+}
+
+variable "runner_name" {
+  description = "The name to give to the GitHub Runner so you can easily identify it"
+  type        = string
+  default     = ""
+}
+
+variable "labels" {
+  description = "A list of additional labels to attach to the runner instance"
+  type        = list(string)
+  default     = ""
 }
