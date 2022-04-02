096b4df374ead72176d66e9ceef21c216b276724
obynodavid12 <obynodavid12@gmail.com>
2022-04-02 16:23:48 -0400

    committing shell

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
-
 set -e
 
-flags_found=false
-
-while getopts t:o: option
-do
-case "${option}"
-in
-t) GITHUBTOKEN=${OPTARG};;
-o) GITHUBORG=${OPTARG};;
-esac
-done
-
-export RUNNER_ALLOW_RUNASROOT="1"
-
 mkdir actions-runner && cd actions-runner
-
 curl -O -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-linux-x64-2.298.2.tar.gz
-
 tar xzf ./actions-runner-linux-x64-2.298.2.tar.gz
+./bin/installdependencies.sh
+mkdir /_work
 
-./config.sh --url "https://github.com/$GITHUBORG" --token $GITHUBTOKEN --runasservice --unattended --replace
+AUTH_HEADER="Authorization: token ${PAT}"
+API_VERSION=v3
+API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
+_FULL_URL="https://api.github.com/repos/${ORG}/${REPO}/actions/runners/registration-token"
+RUNNER_TOKEN="$(curl -XPOST -fsSL \
+  -H "${AUTH_HEADER}" \
+  -H "${API_HEADER}" \
+  "${_FULL_URL}" \
+| jq -r '.token')"
 
-echo "Configured"
-
-sudo ./svc.sh install
-
-echo "Installed"
-
-sudo ./svc.sh start
-
-echo "Started"
+./config.sh --url https://github.com/${ORG}/${REPO} --token "${RUNNER_TOKEN}" --name "aws-runner" --work _work --labels aws-runner --unattended --replace
+./bin/runsvc.sh
