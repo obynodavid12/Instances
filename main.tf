4ca97161b710a75342b58754b0eb8340b6013c14
obynodavid12 <obynodavid12@gmail.com>
2022-04-02 13:39:46 -0400

    committing na

--- main.tf
+++ main.tf
@@ -41,8 +41,15 @@ module "ssh-key" {
 module "ec2" {
   source     = "./modules/ec2"
   namespace  = var.namespace
+  githubtoken = var.GITHUBTOKEN
+  githuborg   = var.GITHUBORG
+  repo        = var.repo
+  aws_default_region = var.AWS_DEFAULT_REGION
+  aws_secret_access_key = var.AWS_SECRET_ACCESS_KEY
+  aws_access_key_id  = var.AWS_ACCESS_KEY_ID
   vpc        = module.networking.vpc
   sg_pub_id  = module.networking.sg_pub_id
   sg_priv_id = module.networking.sg_priv_id
   key_name   = module.ssh-key.key_name
+
 }
