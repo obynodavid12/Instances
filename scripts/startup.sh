1f92f919b933692471a20e5e3bd57bb613fcaa38
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-03 16:11:28 -0400

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,7 +5,7 @@ wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 sudo cp jq /usr/bin
 curl --request POST 'https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token' --header "Authorization: token ${personal_access_token}" > output.txt
-runner_token=\$(jq -r '.token' output.txt)
+echo "runner_token=$(jq -r '.token' output.txt)"
 
 export RUNNER_ALLOW_RUNASROOT="1"
 
