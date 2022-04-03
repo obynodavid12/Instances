e537df95ba37dc17a6580902a982b6b2528aaa1e
obynodavid12 <obynodavid12@gmail.com>
2022-04-03 19:36:18 -0400

    cloudinit

---
+++ cloudinit.tf
@@ -0,0 +1,19 @@
+data "template_cloudinit_config" "cloudinit-example" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    filename     = "init.cfg"
+    content_type = "text/cloud-config"
+    content      = template_file("scripts/init.cfg", {
+      REGION = var.AWS_REGION
+    })
+  }
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = template_file("scripts/runner.sh", {
+    RUNNER_CFG_PAT = var.PERSONAL_ACCESS_TOKEN
+    })
+  }
+}
