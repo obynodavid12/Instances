f507e9c229cb1a74de8f7d6448917e85cdc9da71
obynodavid12 <obynodavid12@gmail.com>
2022-04-03 20:32:02 -0400

    rendered

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -66,14 +66,14 @@ data "template_cloudinit_config" "server-config" {
   part {
     filename     = "init.cfg"
     content_type = "text/cloud-config"
-    content      = template_file("scripts/init.cfg", {
+    content      = templatefile("scripts/init.cfg", {
       region = var.aws_region
     })
   }
 
   part {
     content_type = "text/x-shellscript"
-    content      = template_file("scripts/runner.sh", {
+    content      = templatefile("scripts/runner.sh", {
     personal_access_token = var.personal_access_token
     })
   }
