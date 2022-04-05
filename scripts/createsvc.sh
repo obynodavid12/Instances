13d67ca3abe8d99a9df1d749b9c412e07a0946f7
obynodavid12 <obynodavid12@gmail.com>
2022-04-04 20:03:33 -0400

    evertything

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-set -e
-apt update -y
-apt install git -y
-apt install jq -y
+while getopts t:o: option
+do
+case "${option}"
+in
+t) GITHUBTOKEN=${OPTARG};;
+o) GITHUBORG=${OPTARG};;
+esac
+done
 
-export RUNNER_ALLOW_RUNASROOT=true
+export RUNNER_ALLOW_RUNASROOT="1"
+
 mkdir actions-runner && cd actions-runner
+
 curl -O -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-linux-x64-2.298.2.tar.gz
+
 tar xzf ./actions-runner-linux-x64-2.298.2.tar.gz
-./bin/installdependencies.sh
-mkdir /_work
 
-AUTH_HEADER="Authorization: token ${personal_access_token}"
-API_VERSION=v3
-API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
-_FULL_URL="https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token"
-RUNNER_TOKEN="$(curl -XPOST -fsSL \
-  -H "${AUTH_HEADER}" \
-  -H "${API_HEADER}" \
-  "${_FULL_URL}" \
-| jq -r '.token')"
+./config.sh --url "https://github.com/$GITHUBORG" --token $GITHUBTOKEN --runasservice --unattended --replace
 
-./config.sh --url https://github.com/obynodavid12/Instances --token "${RUNNER_TOKEN}" --name "aws-runner" --work _work --labels aws-runner --unattended --replace
-./bin/runsvc.sh
+echo "Configured"
+
+sudo ./svc.sh install
+
+echo "Installed"
+
+sudo ./svc.sh start
+
+echo "Started"
 
