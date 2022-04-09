a1253938084fbda29af6f736933a26d4d1867f45
obynodavid12 <obynodavid12@gmail.com>
2022-04-08 20:01:07 -0400

    main

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -70,7 +70,7 @@ resource "aws_instance" "selfhosted_runner" {
   vpc_security_group_ids      = [var.sg_priv_id]
 
               
-  user_data = templatefile("scripts/createsvc.sh", {RUNNER_CFG_PAT = var.RUNNER_CFG_PAT})
+  user_data = templatefile("scripts/createsvc.sh", {PERSONAL_ACCESS_TOKEN = var.PERSONAL_ACCESS_TOKEN})
  
   tags = {
     Name = "${var.namespace}-SELFHOSTED-RUNNER"
