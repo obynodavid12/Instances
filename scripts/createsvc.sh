72dfa3af7bfca48bef2e26a99eb87bcd3e553545
obynodavid12 <obynodavid12@gmail.com>
2022-04-08 21:51:43 -0400

    justoky

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -4,17 +4,22 @@ cat <<EOF >/home/ubuntu/user-data.sh
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 sudo cp jq /usr/bin
-curl --request POST 'https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token' --header "Authorization: token ${personal_access_token}" > output.txt
-runner_token=$(jq -r '.token' output.txt)
+registration_url="https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token"
+echo "Requesting registration URL at '${registration_url}'"
+payload=$(curl -sX POST -H "Authorization: token ${personal_access_toekn}" ${registration_url})
+export TOKEN=$(echo $payload | jq .token --raw-output)
 mkdir ~/actions-runner
 cd ~/actions-runner
 curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
-tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
+tar -xzf ./actions-runner-linux-x64-2.289.2.tar.gz
 rm ~/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz
-~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances/ --token \$runner_token --name "DEV-TEST-SELFHOSTED-RUNNER" --unattended
-~/actions-runner/run.sh
+~/actions-runner./config.sh --url https://github.com/obynodavid12/Instances/ --token \$TOKEN --name "selfhosted-runner" --unattended --replace
+~/actions-runner/.run.sh 
 EOF
 cd /home/ubuntu
+chown -R ubuntu
 chmod +x user-data.sh
 /bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
+
+
 
