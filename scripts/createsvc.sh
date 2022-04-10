c6509f8924315565e5f62184b446c1d918340618
obynodavid12 <obynodavid12@gmail.com>
2022-04-10 08:38:27 -0400

    pros

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-cat <<EOF >/home/ubuntu/user-data.sh
-#!/bin/bash
-github_user="obynodavid12"
-github_repo="Instances"
-personal_access_token="${personal_access_token}"
-
-wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-chmod +x ./jq
-sudo cp jq /usr/bin
-token=$(curl -s -XPOST -H "authorization: token $personal_access_token" https://api.github.com/repos/$github_user/$github_repo/actions/runners/registration-token | jq -r .token)
-mkdir ~/actions-runner
-cd ~/actions-runner
-curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz# Optional: Validate the hash
+echo "7ba89bb75397896a76e98197633c087a9499d4c1db7603f21910e135b0d0a238  actions-runner-linux-x64-2.289.2.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
-rm ~/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz
-~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances/ --token \$token --name "DEV-TEST-SELFHOSTED-RUNNER" --unattended
-~/actions-runner/run.sh
-EOF
-cd /home/ubuntu
-chmod +x user-data.sh
-bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
