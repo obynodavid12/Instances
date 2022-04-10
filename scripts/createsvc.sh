19176cd2d5b34a9b9a3cfd08191f9ee33782e601
obynodavid12 <obynodavid12@gmail.com>
2022-04-10 08:26:33 -0400

    clearing

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
-
+cat <<EOF >/home/ubuntu/user-data.sh
+#!/bin/bash
 github_user="obynodavid12"
 github_repo="Instances"
 personal_access_token="${personal_access_token}"
 
-# Download jq for extracting the Token
-apt-get install jq -y
-
-# Create and move to the working directory
-mkdir /actions-runner && cd /actions-runner
-
-# Download the latest runner package
-curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
-
-# Extract the installer
-tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
-
-# Change the owner of the directory to ubuntu
-chown ubuntu -R /actions-runner
-
-# Get the runner's token
+wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x ./jq
+sudo cp jq /usr/bin
 token=$(curl -s -XPOST -H "authorization: token $personal_access_token" https://api.github.com/repos/$github_user/$github_repo/actions/runners/registration-token | jq -r .token)
-
-# Create the runner and start the configuration experience
-sudo -u ubuntu ./config.sh --url https://github.com/obynodavid12/$github_user --token $token --name "github-runner-$(hostname)" --unattended
-
-# Create the runner's service
-./svc.sh install
-
-# Start the service
-./svc.sh start
+mkdir ~/actions-runner
+cd ~/actions-runner
+curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
+tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
+rm ~/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz
+~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances/ --token \$token --name "DEV-TEST-SELFHOSTED-RUNNER" --unattended
+~/actions-runner/run.sh
+EOF
+cd /home/ubuntu
+chmod +x user-data.sh
+bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
