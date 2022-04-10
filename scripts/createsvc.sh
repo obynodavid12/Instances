2752b2430deb7dd1b05071e8e35967e85581e287
obynodavid12 <obynodavid12@gmail.com>
2022-04-10 10:58:04 -0400

    hostname varsy

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -20,8 +20,8 @@ tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
 chown -R ubuntu /home/ubuntu/actions-runner
 
 token=$(curl -s -XPOST -H "authorization: token $personal_access_token" https://api.github.com/repos/$github_user/$github_repo/actions/runners/registration-token | jq -r .token)
-sudo -i -u ubuntu bash << EOF
-/home/ubuntu/actions-runner/config.sh --url https://github.com/obynodavid12/$github_user --token $token --name gh-g2-runner-"self-hosted" --work /home/ubuntu/actions-runner --unattended
-EOF
-./svc.sh install
-./svc.sh start
+
+sudo -i -u ubuntu bash -c 'cd /home/ubuntu/actions-runner/;./config.sh --url https://github.com/obynodavid12/$github_user --token $token --name gh-g2-runner-"self-hosted" --work /home/ubuntu/actions-runner --unattended
+
+
+
