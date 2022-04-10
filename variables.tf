5f5199284bfb272d36be44878e71cb01792efdee
obynodavid12 <obynodavid12@gmail.com>
2022-04-10 07:09:44 -0400

    now fixed

--- variables.tf
+++ variables.tf
@@ -15,8 +15,17 @@ variable "personal_access_token" {
 
 }
 
-variable "github_user" {}
+variable "github_user" {
+  description = "github user name"
+  type        = string
+  default     = "obynodavid12"
+}
 
-variable "github_repo" {}
+variable "github_repo" {
+  description = "Repo name"
+  type  = string
+  default  = "Instances"
+}
+
 
 variable "registration_url" {}
