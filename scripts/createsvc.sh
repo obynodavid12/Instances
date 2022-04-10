94b9ac8a6e9d45118c1370e23f7c5c64f5469bc7
obynodavid12 <obynodavid12@gmail.com>
2022-04-10 08:52:01 -0400

    proscons

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-mkdir actions-runner && cd actions-runner
-curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz# Optional: Validate the hash
-echo "7ba89bb75397896a76e98197633c087a9499d4c1db7603f21910e135b0d0a238  actions-runner-linux-x64-2.289.2.tar.gz" | shasum -a 256 -c
+
+github_user="obynodavid12"
+github_repo="Instances"
+personal_access_token="${personal_access_token}"
+
+# Download jq for extracting the Token
+apt-get install jq -y
+
+# Create and move to the working directory
+mkdir /actions-runner && cd /actions-runner
+
+# Download the latest runner package
+curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
+
+# Extract the installer
 tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
+
+# Change the owner of the directory to ubuntu
+chown ubuntu -R /actions-runner
+
+# Get the runner's token
+token=$(curl -s -XPOST -H "authorization: token $personal_access_token" https://api.github.com/repos/$github_user/$github_repo/actions/runners/registration-token | jq -r .token)
+
+# Create the runner and start the configuration experience
+sudo -u ubuntu ./config.sh --url https://github.com/obynodavid12/$github_user --token $token --name "github-runner-$(hostname)" --unattended
