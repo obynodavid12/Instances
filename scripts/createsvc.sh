1206f247fed9a6765e869cd78b661d60bf2890e9
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-11 08:00:48 -0400

    installdep

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-chmod +x ./jq
-sudo cp jq /usr/bin
-sudo usermod -a -G docker ubuntu
-export RUNNER_ALLOW_RUNASROOT=true
-mkdir actions-runner
-cd actions-runner
+set -e
+
+mkdir actions-runner && cd actions-runner
 curl -O -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
 tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
-PERSONAL_ACCESS_TOKEN=${PERSONAL_ACCESS_TOKEN}
-INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-echo my-runners-$INSTANCEID
-token=$(curl -s -XPOST -H "authorization: token $PERSONAL_ACCESS_TOKEN" https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token | jq -r '.token')
-sudo chown ubuntu -R /actions-runner
-./config.sh --url https://github.com/obynodavid12/Instances --token $token --name "my-runners-$INSTANCEID" --work _work
-sudo ./svc.sh install
-sudo ./svc.sh start
-sudo chown ubuntu -R /actions-runner
+./bin/installdependencies.sh
+mkdir /_work
+
+AUTH_HEADER="Authorization: token ${PERSONAL_ACCESS_TOKEN}"
+API_VERSION=v3
+API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
+_FULL_URL="https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token"
+RUNNER_TOKEN="$(curl -X POST -fsSL \
+  -H "${AUTH_HEADER}" \
+  -H "${API_HEADER}" \
+  "${_FULL_URL}" \
+| jq -r '.token')"
+
+./config.sh --url https://github.com/obynodavid12/Instances --token "${RUNNER_TOKEN}" --name "aws-runner" --work _work --labels aws-runner --unattended --replace
+./bin/runsvc.sh
