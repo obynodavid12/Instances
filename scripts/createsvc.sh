14732dd391d9258ce0fc606fdf145c17ef2fa9df
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-11 08:14:25 -0400

    jsonupdated

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-set -e
-
-mkdir actions-runner && cd actions-runner
-curl -O -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
+cat <<EOF >/home/ubuntu/user-data.sh
+#!/bin/bash
+wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x ./jq
+sudo cp jq /usr/bin
+curl --request POST 'https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token' --header "Authorization: token ${personal_access_token}" > output.json
+runner_token="$(jq -r '.token' output.json)"
+mkdir ~/actions-runner
+cd ~/actions-runner
+curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
 tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
-./bin/installdependencies.sh
-mkdir /_work
+rm ~/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz
+~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances --token "${runner_token}" --name "github-runner" --unattended
+~/actions-runner/run.sh
+EOF
+cd /home/ubuntu
+chmod +x user-data.sh
+/bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
 
-AUTH_HEADER="Authorization: token ${PERSONAL_ACCESS_TOKEN}"
-API_VERSION=v3
-API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
-_FULL_URL="https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token"
-RUNNER_TOKEN="$(curl -X POST -fsSL \
-  -H "${AUTH_HEADER}" \
-  -H "${API_HEADER}" \
-  "${_FULL_URL}" \
-| jq -r '.token')"
 
-./config.sh --url https://github.com/obynodavid12/Instances --token "${RUNNER_TOKEN}" --name "aws-runner" --work _work --labels aws-runner --unattended --replace
-./bin/runsvc.sh
