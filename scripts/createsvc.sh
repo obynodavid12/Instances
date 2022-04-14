4add9e12d380dc335e6d3d4f4a90f304449ef52c
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-14 17:17:27 -0400

    AWS 10

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install curl -y
+cat <<EOF >/home/ubuntu/user-data.sh
+#!/bin/bash
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 sudo cp jq /usr/bin
 RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token | jq .token --raw-output)
-mkdir /home/ubuntu/actions-runner && cd /home/ubuntu/actions-runner || exit
+mkdir ~/actions-runner
+cd ~/actions-runner
 curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
 tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
-chown -R ubuntu /home/ubuntu/actions-runner
-sudo -i -u ubuntu bash << EOF
-/home/ubuntu/actions-runner/config.sh --url https://github.com/obynodavid12/Instances --token ${RUNNER_TOKEN} --name "github-runner" --unattended
+rm ~/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz
+~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances --token ${RUNNER_TOKEN} --name "github-runner" --unattended
+~/actions-runner/run.sh
 EOF
-./svc.sh install
-./svc.sh start
-
-
-
+cd /home/ubuntu
+chmod +x user-data.sh
+exec /bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
 
 
 
