b5db720d77951801474122984c6ebbfdf2ce048e
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-15 10:12:40 -0400

    aws-two

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cat <<EOF >/home/ubuntu/user-data.sh
-#!/bin/bash
+sudo apt-get update
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 sudo cp jq /usr/bin
@@ -9,13 +8,8 @@ cd ~/actions-runner
 curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
 tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
 rm ~/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz
-RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token | jq .token --raw-output)
-~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances --token ${RUNNER_TOKEN} --name "github-runner" --unattended
-~/actions-runner/run.sh
-EOF
-cd /home/ubuntu
-chmod +x user-data.sh
-exec /bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
-
-
-
+echo "Generating token"
+RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token | jq -r '.token')
+./config.sh --url https://github.com/obynodavid12/Instances --token ${RUNNER_TOKEN} --name "github-runner" --unattended --replace
+echo "Running"
+./run.sh
