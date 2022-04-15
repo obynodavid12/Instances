b824559a8a63832d7432610ac92430ede5fb0a02
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-15 11:42:50 -0400

    aws-7

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,16 +1,20 @@
-#!/bin/bash 
-sudo apt-get update -y
+#!/bin/bash
+cat <<EOF >/home/ubuntu/user-data.sh
+#!/bin/bash
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 sudo cp jq /usr/bin
-export RUNNER_ALLOW_RUNASROOT=true
-mkdir actions-runner
-cd actions-runner
-curl -O -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
+curl --request POST 'https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token' --header "Authorization: token ${PERSONAL_ACCESS_TOKEN}" > output.txt
+runner_token="$(jq -r '.token' output.txt)"
+mkdir ~/actions-runner
+cd ~/actions-runner
+curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
 tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
-RUNNER_TOKEN=$(curl -s -XPOST -H "authorization: token $PERSONAL_ACCESS_TOKEN" https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token | jq -r .token)
-sudo chown ubuntu -R /actions-runner
-./config.sh --url https://github.com/obynodavid12/Instances --token $RUNNER_TOKEN --name "my-runner-$(hostname)" --runasservice --unattended --replace 
-./run.sh "$*" &
+rm ~/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz
+~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances --token $runner_token --name "github-runner" --unattended
+~/actions-runner/run.sh
+EOF
+cd /home/ubuntu
+chmod +x user-data.sh
+/bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
 
-wait $!
