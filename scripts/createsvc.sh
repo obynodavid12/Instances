5672ac0ae358f28503fba69c811ecb214650384c
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-15 10:27:08 -0400

    removed tar

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -7,7 +7,6 @@ mkdir ~/actions-runner
 cd ~/actions-runner
 curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
 tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
-rm ~/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz
 echo "Generating token"
 RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token | jq -r '.token')
 ./config.sh --url https://github.com/obynodavid12/Instances --token ${RUNNER_TOKEN} --name "github-runner" --unattended --replace
