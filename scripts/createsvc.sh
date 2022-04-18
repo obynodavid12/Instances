a5722eaae9235da7a2a5881917cd4e6b748077cd
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-18 10:28:34 -0400

    doings

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-sudo apt update -y
+cat <<EOF >/home/ubuntu/user-data.sh
+#!/bin/bash
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 sudo cp jq /usr/bin
-sudo -u ubuntu mkdir /home/ubuntu/actions-runner
-sudo -u ubuntu curl -o /home/ubuntu/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
-sudo -u ubuntu tar xzf /home/ubuntu/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz -C /home/ubuntu/actions-runner
-sudo -u ubuntu EC2_INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id` bash -c 'cd /home/ubuntu/actions-runner/;./config.sh --url https://github.com/obynodavid12/Instances  --PERSONAL_ACCESS_TOKEN ${PERSONAL_ACCESS_TOKEN} --name "github-runner"-$${EC2_INSTANCE_ID}" --unattended'
-
-# start the github runner as a service on startup
-cd /home/ubuntu/actions-runner/;./svc.sh install
-cd /home/ubuntu/actions-runner/;./svc.sh start
-
+RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token | jq .token --raw-output)
+mkdir ~/actions-runner
+cd ~/actions-runner
+curl -o actions-runner-linux-x64-2.290.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.290.1/actions-runner-linux-x64-2.290.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.290.1.tar.gz
+rm ~/actions-runner/actions-runner-linux-x64-2.290.1.tar.gz
+~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances/ --token ${RUNNER_TOKEN} --name "github-runner" --unattended
+~/actions-runner/run.sh
+EOF
+cd /home/ubuntu
+chmod +x user-data.sh
+/bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
 
