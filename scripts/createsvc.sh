1a59ea4697f9d1577ebd132d2200221e5a33831e
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-18 08:53:26 -0400

    hehehe

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-createsvc="createsvc.sh"
-chmod a+rwx "$createsvc"
+sudo apt update -y
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 sudo cp jq /usr/bin
-mkdir ~/actions-runner
-cd ~/actions-runner
-chown -R ubuntu /home/ubuntu/actions-runner
-curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
-tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
-RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token | jq .token --raw-output)
-./config.sh --url https://github.com/obynodavid12/Instances --token ${RUNNER_TOKEN} --name "github-runner" --unattended
-./run.sh & wait $!
+sudo -u ubuntu mkdir /home/ubuntu/actions-runner
+sudo -u ubuntu curl -o /home/ubuntu/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
+sudo -u ubuntu tar xzf /home/ubuntu/actions-runner/actions-runner-linux-x64-2.289.2.tar.gz -C /home/ubuntu/actions-runner
+sudo -u ubuntu EC2_INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id` bash -c 'cd /home/ubuntu/actions-runner/;./config.sh --url https://github.com/obynodavid12/Instances  --PERSONAL_ACCESS_TOKEN ${PERSONAL_ACCESS_TOKEN} --name "${runner_name}-$${EC2_INSTANCE_ID}" --work _work --labels ${labels} --runasservice'
+
+# start the github runner as a service on startup
+cd /home/ubuntu/actions-runner/;./svc.sh install
+cd /home/ubuntu/actions-runner/;./svc.sh start
 
 
