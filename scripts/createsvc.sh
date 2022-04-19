2c2c683835b02e0af3947e6d76afcd9f0abdf62f
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-19 13:22:56 -0400

    okdot

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-cat <<EOF >/home/ubuntu/user-data.sh
-#!/bin/bash
+sudo apt-get update -y
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
-sudo cp jq /usr/bin
-RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/obynodavid12/Instances/actions/runners/registration-token | jq .token --raw-output)
-mkdir ~/actions-runner
-cd ~/actions-runner
-curl -o actions-runner-linux-x64-2.290.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.290.1/actions-runner-linux-x64-2.290.1.tar.gz
-tar xzf ./actions-runner-linux-x64-2.290.1.tar.gz
-rm ~/actions-runner/actions-runner-linux-x64-2.290.1.tar.gz
-~/actions-runner/config.sh --url https://github.com/obynodavid12/Instances/ --token ${RUNNER_TOKEN} --name "github-runner" --unattended
-~/actions-runner/run.sh
-EOF
-cd /home/ubuntu
-chmod +x user-data.sh
-/bin/su -c "./user-data.sh" - ubuntu | tee /home/ubuntu/user-data.log
+sudo mv jq /usr/bin
+export RUNNER_CFG_PAT=${{ secrets.RUNNER_CFG_PAT }}
+curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/create-latest-svc.sh | bash -s -- -s obynodavid12/Instances -n gbrunner -l gpu,x64,linux
 
