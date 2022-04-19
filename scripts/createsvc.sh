c04f36f195365d4fe396bfd4cfc3703951c6a12b
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-04-19 13:38:53 -0400

    Update createsvc.sh

--- scripts/createsvc.sh
+++ scripts/createsvc.sh
@@ -3,6 +3,6 @@ sudo apt-get update -y
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
 sudo mv jq /usr/bin
-export RUNNER_CFG_PAT="${{ secrets.RUNNER_CFG_PAT }}"
+export "RUNNER_CFG_PAT=${{ secrets.RUNNER_CFG_PAT }}"
 curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/create-latest-svc.sh | bash -s -- -s obynodavid12/Instances -n gbrunner -l gpu,x64,linux
 
